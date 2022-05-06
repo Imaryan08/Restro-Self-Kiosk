63112a9752c838e9b20aff942dd5b4bf08dd9f98
Rampukar <imaryan08@gmail.com>
2022-05-06 23:25:08 +0530

    restro

--- mcdi.js
+++ mcdi.js
@@ -1,301 +1,151 @@
-let burger = [
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626342718/Item/5276.png",
-    name: "Triple Cheese American Veg Burger",
-    cal: "120 Cal",
-    price: "₹ 201",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626242155/Item/5294.png",
-    name: "McSpicy Premium Veg Burger",
-    cal: "180 Cal",
-    price: "₹ 221",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626682653/Item/5412.png",
-    name: "Piri Piri Spiced Veg Burger",
-    cal: "80 Cal",
-    price: "₹ 226",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626342713/Item/5275.png",
-    name: "Triple Cheese American Chicken Burger",
-    cal: "140 Cal",
-    price: "₹ 231",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626242155/Item/5275.png",
-    name: "McChicken Burger ",
-    cal: "110 Cal",
-    price: "₹ 126",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626242155/Item/5294.png",
-    name: "McSpicy Chicken Burger ",
-    cal: "200 Cal",
-    price: "₹ 173",
-  },
-];
+let burgers = JSON.parse(localStorage.getItem('burger'));
+let beverages = JSON.parse(localStorage.getItem('beverages'));
+let desserts = JSON.parse(localStorage.getItem('desserts'));
+let favMeals = JSON.parse(localStorage.getItem('favMeal'));
 
-localStorage.setItem("burger", JSON.stringify(burger));
-
-let beverages = [
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1589466119/Item/16.png",
-    name: "Coke",
-    cal: "80 Cal",
-    price: "₹ 100",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1593066950/Item/3670.png",
-    name: "MIXED BERRY SMOTTHIE",
-    cal: "148 Cal",
-    price: "₹ 184",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1636382893/Item/6090.png",
-    name: "Mocha Oreo Shake",
-    cal: "115 Cal",
-    price: "₹ 247",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1636382883/Item/6091.png",
-    name: "Strawberry Oreo Shake",
-    cal: "120 Cal",
-    price: "₹ 267",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1589467260/Item/30.png",
-    name: "Sprite",
-    cal: "120 Cal",
-    price: "₹ 100",
-  },
-];
-
-localStorage.setItem("beverages", JSON.stringify(beverages));
-
-let desserts = [
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1589466970/Item/3673.png",
-    name: "Chocolate Chip Muffin",
-    cal: "127 Cal",
-    price: "₹ 121",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1621519296/Item/5911.png",
-    name: "Black Forest Mcflurry BOGO",
-    cal: "84 Cal",
-    price: "₹ 117",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1589466116/Item/3.png",
-    name: "McFlurry Oreo Medium",
-    cal: "132 Cal",
-    price: "₹ 112",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1589466113/Item/5.png",
-    name: "Soft Serve Hot Fudge",
-    cal: "110 Cal",
-    price: "₹ 89",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1642087175/Item/4.png",
-    name: "Mcflurry Oreo Small",
-    cal: "60 Cal",
-    price: "₹ 84",
-  },
-];
-
-localStorage.setItem("desserts", JSON.stringify(desserts));
-
-let favMeal = [
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626342720/Item/5279.png",
-    name: "Triple Cheese American Chicken Meal",
-    cal: "140 Cal",
-    price: "₹ 320",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626342720/Item/5280.png",
-    name: "Triple Cheese American Veg Meal",
-    cal: "124 Cal",
-    price: "₹ 320",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1637909065/Item/5283.png",
-    name: "McSpicy Premium Chicken Meal",
-    cal: "180 Cal",
-    price: "₹ 320",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1637909065/Item/5295.png",
-    name: "McSpicy Premium Veg Meal",
-    cal: "157 Cal",
-    price: "₹ 220",
-  },
-  {
-    img_url:
-      "https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_250/v1626682653/Item/5398.png",
-    name: "Piri Piri Spiced Veg Burger Meal",
-    cal: "160 Cal",
-    price: "₹ 220",
-  },
-];
-
-localStorage.setItem("favMeal", JSON.stringify(favMeal));
-let burgers = JSON.parse(localStorage.getItem("burger"));
-let beverages = JSON.parse(localStorage.getItem("beverages"));
-let desserts = JSON.parse(localStorage.getItem("desserts"));
-let favMeals = JSON.parse(localStorage.getItem("favMeal"));
-
-function beverage() {
-  createSubMenu(beverages);
+function beverage(){
+    createSubMenu(beverages);
 }
-function burger() {
-  createSubMenu(burgers);
+function burger(){
+    createSubMenu(burgers);
 }
-function favMeal() {
-  createSubMenu(favMeals);
+function favMeal(){
+    createSubMenu(favMeals);
 }
-function dessert() {
-  createSubMenu(desserts);
+function dessert(){
+    createSubMenu(desserts);
 }
 
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
 
 let sum = 0;
 let count = 0;
 let i = 5;
 
-function createSubMenu(x) {
-  let subMenu = document.querySelector(".subMenu");
-  subMenu.innerHTML = null;
-  subMenu.style.background = "#fff";
 
-  x.map(function (elem) {
-    let div = document.createElement("div");
-    let detailDiv = document.createElement("div");
+function createSubMenu(x){
+    let subMenu = document.querySelector('.subMenu');
+    subMenu.innerHTML = null;
+    subMenu.style.background = '#fff';
 
-    let imgDiv = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = elem.img_url;
-    imgDiv.append(img);
+    x.map(function(elem){
 
-    let name = document.createElement("h2");
-    name.innerHTML = elem.name;
+        let div = document.createElement('div');
+        let detailDiv = document.createElement('div');
+        
+        let imgDiv = document.createElement('div');
+        let img = document.createElement('img');
+        img.src = elem.img_url;
+        imgDiv.append(img);
 
-    let cal = document.createElement("h4");
-    cal.innerHTML = elem.cal;
+        let name = document.createElement('h2');
+        name.innerHTML = elem.name;
 
-    let price = document.createElement("h5");
-    price.innerHTML = elem.price;
+        let cal = document.createElement('h4');
+        cal.innerHTML = elem.cal;
+        
+        let price = document.createElement('h5');
+        price.innerHTML = elem.price;
 
-    let addToOrder = document.createElement("button");
-    addToOrder.innerHTML = "Add to order";
+        let addToOrder = document.createElement('button');
+        addToOrder.innerHTML = 'Add to order';
 
-    // append
-    detailDiv.append(name, cal, price, addToOrder);
-    div.append(imgDiv, detailDiv);
-    document.querySelector(".subMenu").append(div);
+        // append
+        detailDiv.append(name, cal, price, addToOrder);
+        div.append(imgDiv,detailDiv);
+        document.querySelector('.subMenu').append(div);
 
-    // Add to order
-    addToOrder.addEventListener("click", function () {
-      addToOrder.innerHTML = "Added";
-      addToOrder.style.background = "red";
-      cart.push(elem);
-      localStorage.setItem("cart", JSON.stringify(cart));
-      let total = document.querySelector(".total");
-      let item = document.querySelector(".item");
+        // Add to order
+        addToOrder.addEventListener('click',function(){
+            addToOrder.innerHTML = 'Added';
+            addToOrder.style.background = 'red';
+            cart.push(elem);
+            localStorage.setItem('cart',JSON.stringify(cart));
+            let total = document.querySelector('.total');
+            let item = document.querySelector('.item');
 
-      //Removing all the chracters except number
-      let str = elem.price;
-      str = +str.replace(/[^0-9]/g, "");
-      sum += str;
+            //Removing all the chracters except number
+            let str = elem.price;
+            str = +str.replace(/[^0-9]/g, '');
+            sum += str;
+            
+            total.innerHTML = `₹${sum}`;
+            item.innerHTML = `${++count}`;
 
-      total.innerHTML = `₹${sum}`;
-      item.innerHTML = `${++count}`;
-    });
-  });
+        });
+    })
 }
 
 //Order Food
-document.querySelector(".orderFood").addEventListener("click", function () {
-  let myPromise = new Promise(function (resolve, reject) {
-    if (sum && count) {
-      resolve(`Your order will be ready in ${i} seconds`);
-    } else {
-      reject("Please add something in your order");
-    }
-  });
+document.querySelector('.orderFood').addEventListener('click',function(){
 
-  myPromise.then(function (res) {
-    let id = setInterval(function () {
-      message(`Your order will be ready in ${i} seconds`, "green");
-      if (i === 0) {
-        clearInterval(id);
-        let subMenu = document.querySelector(".subMenu");
-        subMenu.innerHTML = null;
+    let myPromise = new Promise(function (resolve, reject){
+        if(sum  && count ){
+            resolve(`Your order will be ready in ${i} seconds`);
+        }else{
+            reject("Please add something in your order");
+        }
+    })
 
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        cart.map(function (elem) {
-          let img = document.createElement("img");
-          img.src = elem.img_url;
-          img.style.boxShadow =
-            "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px";
-          img.style.margin = "10px 10px 10px 40px";
-          img.style.padding = "20px 0";
-          img.style.width = "200px";
-          subMenu.style.background = "#fff";
+    myPromise.then(function(res){
+        let id = setInterval(function(){
+            message(`Your order will be ready in ${i} seconds`,'green');
+            if(i === 0){
+                clearInterval(id);
+                let subMenu = document.querySelector('.subMenu');
+                subMenu.innerHTML = null;
 
-          let para = document.createElement("p");
-          para.setAttribute("class", "orderId");
-          para.innerHTML = `<span style='color: green; font-size: 16px;'> Thank you 🎉 </span>Your Order ID: ${Math.round(
-            Math.random() * 100000000
-          )}`;
-          subMenu.append(para, img);
-          document.querySelector(".orderId").style.display = "block";
+                let cart = JSON.parse(localStorage.getItem('cart'));
+                cart.map(function(elem){
+                    let img = document.createElement('img');
+                    img.src = elem.img_url;
+                    img.style.boxShadow = 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px';
+                    img.style.margin = '10px 10px 10px 40px';
+                    img.style.padding = '20px 0';
+                    img.style.width = '200px';
+                    subMenu.style.background = '#fff';
 
-          localStorage.removeItem("cart");
-        });
-      }
-      i--;
-    }, 1000);
-  });
+                    let para = document.createElement('p');
+                    para.setAttribute('class','orderId');
+                    para.innerHTML = `<span style='color: green; font-size: 16px;'> Thank you 🎉 </span>Your Order ID: ${Math.round(Math.random()*100000000)}`;
+                    subMenu.append(para,img);
+                    document.querySelector('.orderId').style.display = 'block';
 
-  myPromise.catch(function (err, red) {
-    message(err, "red");
-  });
-});
+                    localStorage.removeItem('cart');
+                })
+                
 
-function message(x, y) {
-  document.querySelector(".subMenu").innerHTML = null;
-  let h1 = document.createElement("h1");
-  h1.innerHTML = x;
-  let subMenu = document.querySelector(".subMenu");
-  subMenu.append(h1);
-  h1.style.textAlign = "center";
-  h1.style.padding = "10px";
-  h1.style.color = y;
-  subMenu.style.background = "black";
+
+            }
+            i--;
+        },1000)
+    })
+
+    myPromise.catch(function(err,red){
+        message(err,'red');
+    })
+
+})
+
+
+
+function message(x,y){
+    document.querySelector('.subMenu').innerHTML = null;
+        let h1 = document.createElement('h1');
+        h1.innerHTML = x;
+        let subMenu = document.querySelector('.subMenu');
+        subMenu.append(h1)
+        h1.style.textAlign = 'center';
+        h1.style.padding = '10px';
+        h1.style.color = y;
+        subMenu.style.background = 'black';
 }
+
+
+
+
+
